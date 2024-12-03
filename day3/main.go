d120fa21fcba8df5ce02ac94b082619b21fbf37c
Felix Neumann <dev@fxnn.de>
2024-12-03 10:03:42 +0100

    day2: solved part 2

--- day3/main.go
+++ day3/main.go
@@ -17,12 +17,20 @@ func main() {
 		text += scanner.Text()
 	}
 
-	pattern := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	pattern := regexp.MustCompile(`(do|don't)\(\)|mul\((\d+),(\d+)\)`)
 	result := 0
+	do := true
 	for _, match := range pattern.FindAllStringSubmatch(text, -1) {
-		n1 := util.Atoi(match[1])
-		n2 := util.Atoi(match[2])
-		result += n1 * n2
+		fmt.Printf("%v\n", match)
+		if match[1] == "do" {
+			do = true
+		} else if match[1] == "don't" {
+			do = false
+		} else if do {
+			n1 := util.Atoi(match[2])
+			n2 := util.Atoi(match[3])
+			result += n1 * n2
+		}
 	}
 
 	fmt.Printf("result: %d\n", result)

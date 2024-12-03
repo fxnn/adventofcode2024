a6da24f2101912b2339d79677095092aa885eb9b
Felix Neumann <dev@fxnn.de>
2024-12-03 01:15:14 +0100

    add day2

---
+++ day2/day2.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/fxnn/adventofcode2024/util"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func levels(report string) []int {
+	strVals := strings.Split(report, " ")
+	levels := make([]int, len(strVals))
+	for i := range strVals {
+		var err error
+		levels[i], err = strconv.Atoi(strVals[i])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error in Atoi: %s\n", err)
+			os.Exit(1)
+		}
+	}
+	return levels
+}
+
+func isSafe(levels []int, index int) bool {
+	firstDiff := levels[0] - levels[1]
+	diff := levels[index-1] - levels[index]
+	if diff == 0 || util.Abs(diff) > 3 {
+		fmt.Printf("  unsafe diff (%d) at index %d\n", diff, index)
+		return false
+	}
+	if util.Sign(diff) != util.Sign(firstDiff) {
+		fmt.Printf("  unsafe sign (%d,%d) at index %d\n", util.Sign(diff), util.Sign(firstDiff), index)
+		return false
+	}
+	return true
+}
+
+func firstUnsafeIndex(levels []int) int {
+	for i := range levels {
+		if i > 0 {
+			if !isSafe(levels, i) {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	safeReports := 0
+	dampenedSafeReports := 0
+	for scanner.Scan() {
+		report := scanner.Text()
+		levels := levels(report)
+		fmt.Printf("report %v\n", report)
+
+		index := firstUnsafeIndex(levels)
+		if index == -1 {
+			safeReports++
+			dampenedSafeReports++
+			fmt.Printf("  safe\n")
+		} else {
+			var err error
+			levels, err = util.RemoveElement(levels, index)
+			fmt.Printf("  dampened %v\n", levels)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error in RemoveElement: %s\n", err)
+				os.Exit(1)
+			}
+			if firstUnsafeIndex(levels) == -1 {
+				dampenedSafeReports++
+				fmt.Printf("  safe with dampener\n")
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("safe reports: %d\n", safeReports)
+	fmt.Printf("dampened safe reports: %d\n", dampenedSafeReports)
+}
--- util/util.go
+++ util/util.go
@@ -1,9 +1,38 @@
 package util
 
+import "fmt"
+
 // Abs calculates the absolute value of an integer
 func Abs(x int) int {
-	if x > 0 {
+	if x < 0 {
 		return -x
 	}
 	return x
+}
+
+// Sign returns -1, 0 or 1 depending on the sign of the given integer
+func Sign(x int) int {
+	if x > 0 {
+		return 1
+	}
+	if x == 0 {
+		return 0
+	}
+	return -1
+}
+
+// RemoveElement removes element with given index from slice
+func RemoveElement(slice []int, index int) ([]int, error) {
+	if index < 0 || index >= len(slice) {
+		return nil, fmt.Errorf("index out of range")
+	}
+
+	// Create a new slice and copy the elements before the index
+	newSlice := make([]int, 0, len(slice)-1)
+	newSlice = append(newSlice, slice[:index]...)
+
+	// Append the elements after the index
+	newSlice = append(newSlice, slice[index+1:]...)
+
+	return newSlice, nil
 }
